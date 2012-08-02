d6276d1f6b8d2e950adda6cad8e276d535907343
zachi <zachi.greenberger@gmail.com>
2012-08-02 11:09:04 +0300

    first version

---
+++ zachi.me/Scripts/site.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+  $('.category').click(function (event) {
+    var isSimon = !$(this).hasClass('category-mini');
+    if (isSimon) {
+      $('.categories').addClass('show-category')
+      var categoryId = $(this).attr('id');
+      $('#' + categoryId + '-page').show();
+    }
+    else {
+      $('.categories').removeClass('show-category')
+      $('.page').hide();
+    }
+
+
+    $('.category').each(function (index, element) {
+      if (isSimon) {
+        $(element).addClass('category-mini')
+      }
+      else {
+        $(element).removeClass('category-mini')
+      }
+
+    })
+  })
+})
