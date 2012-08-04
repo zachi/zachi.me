60d2af1829fb870109375d111db14954ae87fc7f
zachi <zachi.greenberger@gmail.com>
2012-08-04 12:03:44 +0300

    progress...

--- zachi.me/Scripts/site.js
+++ zachi.me/Scripts/site.js
@@ -1,15 +1,34 @@
 ﻿$(function () {
-  $('.simon-button').click(function (event) {
-    var isSimon = !$(this).hasClass('simon-button-mini');
-    if (isSimon) {
-      $('.categories').addClass('show-category')
-      $(this).siblings().eq(0).show();
-      $('.simon-button').addClass('simon-button-mini');
-    }
-    else {
-      $('.categories').removeClass('show-category')
-      $('.page').hide();
-      $('.simon-button').removeClass('simon-button-mini')
-    }
-  })
+  $('.simon-button').click(simonApp.simonButtonClickHandler)
+  $('.name-title').click(simonApp.nameTitleClickHandler);
 })
+var simonApp = (function () {
+
+  return {
+    simonButtonClickHandler: function (event) {
+
+      var isSimonButtonMini = $(this).hasClass('simon-button-mini');
+      if (isSimonButtonMini) {
+        $.proxy(simonApp.simonButtonMiniClickHandler, this)();
+        return;
+      }
+      $('.categories').addClass('single-category-opened')
+      //$(this).siblings().eq(0).show();
+      $(this).siblings().eq(0).addClass('page-visible');
+      $('.simon-button').addClass('simon-button-mini');
+      $(this).addClass('selected');
+    },
+    nameTitleClickHandler: function (event) {
+      $('.categories').removeClass('single-category-opened')
+      $('.page').removeClass('page-visible');
+      $('.simon-button').removeClass('simon-button-mini').removeClass('selected')
+    },
+    simonButtonMiniClickHandler: function () {
+      //console.log(this);
+      $('.page-visible').removeClass('page-visible');
+      $('.simon-button').removeClass('selected')
+      $(this).addClass('selected')
+      $(this).siblings().eq(0).addClass('page-visible');
+    }
+  };
+})();
